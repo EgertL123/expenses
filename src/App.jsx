2d6982f18b161738110aecd6e30701cda4f86a38
Egert Linnas <egert.linnas@voco.ee>
2025-01-23 13:26:17 +0200

    Inserted data into component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+  {
+    date: new Date(2024, 10, 12),
+    title: 'New book',
+    price: 30.99
+  },
+  {
+    date: new Date(2024, 10, 12),
+    title: 'New jeans',
+    price: 99.99
+  }
+] 
   return (
     <div classname="App">
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   );
 }

420dac274f671184fe504e69a8aa872e30ebd7fc
Egert Linnas <egert.linnas@voco.ee>
2025-01-31 13:36:05 +0200

    Komponentide kompositsioon ja info edastamine

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -4,7 +4,7 @@ import ExpenseDate from './ExpenseDate'
 const ExpenseItem = (props) => {
 
     return (
-        <div className='card expense-item'>
+        <div className='expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className='expense-item_description'>
                 <h2>{props.data.title}</h2>

a984cf40ca7dc924f1317b2965767164e3d28409
Egert Linnas <egert.linnas@voco.ee>
2025-01-31 13:04:46 +0200

    Ymbritseva komponendi rakendamine teistes komponentides (yl 3)

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -4,7 +4,7 @@ import ExpenseDate from './ExpenseDate'
 const ExpenseItem = (props) => {
 
     return (
-        <div className='expense-item'>
+        <div className='card expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className='expense-item_description'>
                 <h2>{props.data.title}</h2>
